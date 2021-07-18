7d8940bb1277d5fd13b41dde891af5b8c11fb539
Vinicius Ventura <vinicius__20@hotmail.com>
2021-07-18 19:44:20 -0300

    add events and DOM manipulation

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    const content = document.getElementById("content");
+    const loader = document.getElementById("loader");
+    const sended = document.getElementById("sended");
+
+    const data = {
+        name,
+        email
+    };
+
+    const convertData = JSON.stringify(data);
+
+    localStorage.setItem("lead", convertData);
+
+    content.style.display = "none";
+    loader.style.display = "block"
+
+    setTimeout(() => {
+        loader.style.display = "none";
+        sended.style.display = "block"
+    }, 1000)
+
+})
